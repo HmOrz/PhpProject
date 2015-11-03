bb3f2fe27d2f4f781dd115e0a674db2848168c03
HmOrz <xhmming@foxmail.com>
2015-11-04 00:41:42 +0800

    注册模块完善

---
+++ UI/register.php
@@ -0,0 +1,65 @@
+﻿<?
+    error_reporting(E_ALL & ~E_DEPRECATED);
+	$no = trim($_POST['id']);
+	$password = $_POST['password'];
+	$cpassword = $_POST['cpassword'];
+	$name = trim($_POST['name']);
+	$email = trim($_POST['email']);
+	$sex = $_POST['sex'];
+	$nation = trim($_POST['nation']);
+	$born = trim($_POST['born']);
+	$indate = trim($_POST['in_date']);
+	$party = trim($_POST['party']);
+	$major = trim($_POST['major']);
+	$iden = trim($_POST['iden']);
+	$phone = trim($_POST['phonenum']);
+	$address = $_POST['address'];
+	echo $sex;
+	if (empty($no) || empty($born) || empty($password) || $cpassword != $password || empty($indate)
+		 || empty($indate) || empty($major) || empty($phone) || empty($address) || empty($name))
+	{
+		echo '数据输入不完整';
+		exit;
+	}
+	else
+	{
+		if (strlen($password) < 6 || strlen($password) > 30)
+		{
+			echo '密码必须在6到30个字符之间';
+			exit;
+		}
+		$pattern = "/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/";
+		if(!preg_match($pattern, $email))
+		{
+			echo 'Email格式不合法！';
+			exit;
+		}
+		$db = mysql_connect('localhost','root');
+	    mysql_select_db("clprj");
+		//$db = mysql_connect('localhost','root','198251') or die('Could not connect: '.mysql_error());
+		//mysql_select_db('register') or die('Could not select database');
+		$sql = "SELECT * FROM `user` WHERE `no` = '".$no."'";
+		//echo $sql;
+		$result = mysql_query($sql);
+		if ($result && mysql_num_rows($result) > 0)
+		{
+			echo "<font color='red' size='5'>该学号已被注册！</font><br>\n";
+		}
+		else
+		{
+			$sql = "INSERT INTO `user` (no,name,pwd,sex,nation,born,indate,party,phone,major,adress,email) VALUES";
+			$sql .= "('$no', '$name', '$password', '$sex', '$nation', '$born', '$indate', '$party', '$phone', '$major', '$address', '$email')";
+			$result = mysql_query($sql);
+			echo $sql;
+			if(!$result)
+			{
+				mysql_free_result($result);
+				mysql_close($db);
+				echo '数据记录插入失败！';
+				exit;
+			}
+			echo "<font color='red' size='5'>恭喜您注册成功！</font><br>\n";
+		}
+		mysql_close($db);
+	}
+?>
